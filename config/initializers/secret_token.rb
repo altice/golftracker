4d58d74b9988128bfa63b07dfa1fe34c68168d20
MakerSquare Student <students@themakersquare.com>
2014-01-08 22:44:03 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Golftracker::Application.config.secret_key_base = '3998e60d359e57e3e59ac5c963df82025af9aa763fb4861a44b480243060220dfe4644f334585aefefe985cb4df25bd70a4335eda542de070bad6ac886764fa9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Golftracker::Application.config.session_store :cookie_store, key: '_golftracker_session'
